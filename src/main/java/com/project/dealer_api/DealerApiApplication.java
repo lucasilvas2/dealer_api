c0a051e7c9b673d711d165e2395d3044fd2d85e9
lucasilvas2 <lucasoliver1068@gmail.com>
2022-11-18 17:52:20 -0300

    fix: Renomeando projeto

---
+++ src/main/java/com/project/dealer_api/DealerApiApplication.java
@@ -0,0 +1,13 @@
+package com.project.dealer_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DealerApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DealerApiApplication.class, args);
+    }
+
+}

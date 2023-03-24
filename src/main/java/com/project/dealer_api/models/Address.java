0610d09e8d8ca86eac83a487c3fbed0338cfa6f8
lucasilvas2 <lucasoliver1068@gmail.com>
2023-03-24 18:40:09 -0300

    feat: Create Address repository

--- src/main/java/com/project/dealer_api/models/Address.java
+++ src/main/java/com/project/dealer_api/models/Address.java
@@ -11,7 +11,6 @@ public class Address {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
-
     private String street;
     private String number;
     private String district;
---
+++ src/main/java/com/project/dealer_api/repository/AddressRepository.java
@@ -0,0 +1,19 @@
+package com.project.dealer_api.repository;
+
+import com.project.dealer_api.models.Address;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface AddressRepository extends JpaRepository<Address, Integer> {
+
+    List<Address> findByStreet(String street);
+    List<Address> findByNumber(String number);
+    List<Address> findByCity(String city);
+    List<Address> findByPostalCode(String postalCode);
+    List<Address> findByDistrict(String district);
+    List<Address> findByStreetAndNumber(String street, String number);
+
+}

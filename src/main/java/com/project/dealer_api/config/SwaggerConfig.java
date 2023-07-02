4d6b8f93c06e49d7573c3de50f53c26612c85b82
lucasilvas2 <lucasoliver1068@gmail.com>
2023-07-02 01:27:36 -0300

    feat: Add oauth2 security config

--- src/main/java/com/project/dealer_api/config/SwaggerConfig.java
+++ src/main/java/com/project/dealer_api/config/SwaggerConfig.java
@@ -1,23 +1,106 @@
 package com.project.dealer_api.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import springfox.documentation.builders.PathSelectors;
-import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.builders.*;
+import springfox.documentation.service.AuthorizationScope;
+import springfox.documentation.service.GrantType;
+import springfox.documentation.service.SecurityReference;
+import springfox.documentation.service.SecurityScheme;
 import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spi.service.contexts.SecurityContext;
 import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger.web.SecurityConfiguration;
+import springfox.documentation.swagger.web.SecurityConfigurationBuilder;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
 
 @Configuration
 @EnableSwagger2
 public class SwaggerConfig {
 
+//    @Bean
+//    public Docket api(){
+//        return new Docket(DocumentationType.SWAGGER_2)
+//                .select()
+//                .apis(RequestHandlerSelectors.any())
+//                .paths(PathSelectors.any())
+//                .build();
+//    }
+//
+    @Value("${security.oauth2.client.client-id}")
+    private String clientId;
+
+    @Value("${security.oauth2.client.client-secret}")
+    private String clientSecret;
+
     @Bean
-    public Docket api(){
+    public Docket api() {
         return new Docket(DocumentationType.SWAGGER_2)
                 .select()
                 .apis(RequestHandlerSelectors.any())
                 .paths(PathSelectors.any())
+                .build()
+                .securitySchemes(Collections.singletonList(securityScheme()))
+                .securityContexts(Collections.singletonList(securityContext()));
+    }
+
+    private SecurityScheme securityScheme() {
+        GrantType grantType = new AuthorizationCodeGrantBuilder()
+                .tokenEndpoint(new TokenEndpointBuilder().url("/oauth/token").build())
+                .tokenRequestEndpoint(
+                        new TokenRequestEndpointBuilder().url("/oauth/authorize").clientIdName("clientId").clientSecretName("clientSecret").build())
+                .build();
+
+        SecurityScheme oauth = new OAuthBuilder().name("oauth2")
+                .grantTypes(Collections.singletonList(grantType))
+                .scopes(Arrays.asList(scopes()))
+                .build();
+
+        return oauth;
+    }
+
+    private AuthorizationScope[] scopes() {
+        AuthorizationScope[] scopes = {
+                new AuthorizationScope("read", "Read access"),
+                new AuthorizationScope("write", "Write access"),
+        };
+        return scopes;
+    }
+
+    private SecurityContext securityContext() {
+        return SecurityContext.builder()
+                .securityReferences(defaultAuth())
+                .forPaths(PathSelectors.any())
                 .build();
     }
+
+    private List<SecurityReference> defaultAuth() {
+        AuthorizationScope[] scopes = {
+                new AuthorizationScope("read", "Read access"),
+                new AuthorizationScope("write", "Write access")
+        };
+
+        return Collections.singletonList(
+                SecurityReference.builder()
+                        .reference("oauth2")
+                        .scopes(scopes)
+                        .build()
+        );
+    }
+
+    @Bean
+    public SecurityConfiguration security() {
+        return SecurityConfigurationBuilder.builder()
+                .clientId(clientId)
+                .clientSecret(clientSecret)
+                .realm("realm")
+                .appName("appName")
+                .build();
+    }
+
 }

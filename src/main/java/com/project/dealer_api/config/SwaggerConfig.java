7bb9699db6743e77350696ee67f2949d7a19935e
lucasilvas2 <lucasoliver1068@gmail.com>
2023-07-30 23:15:20 -0300

    style: delete comments in code

--- src/main/java/com/project/dealer_api/config/SwaggerConfig.java
+++ src/main/java/com/project/dealer_api/config/SwaggerConfig.java
@@ -22,11 +22,6 @@ public class SwaggerConfig {
 
     @Bean
     public Docket api(){
-//        return new Docket(DocumentationType.SWAGGER_2)
-//                .select()
-//                .apis(RequestHandlerSelectors.any())
-//                .paths(PathSelectors.any())
-//                .build();
         return new Docket(DocumentationType.SWAGGER_2)
                 .select()
                 .paths(PathSelectors.any())
